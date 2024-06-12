76e62d0f290c96e931f164dc26fc185a40c46562
xiaoxin <853006022@qq.com>
2024-06-12 17:57:46 +0800

    update dio——utils

---
+++ lib/utils/currency_utils.dart
@@ -0,0 +1,18 @@
+
+import 'package:intl/intl.dart';
+
+class CurrencyUtils {
+
+  static String formatCurrency(double value) {
+    return value.toStringAsFixed(2);
+  }
+
+  static String formatCurrencyWithComma(dynamic money) {
+
+    var formatter = NumberFormat('#,##0.00', 'en_US');
+    return formatter.format(money.truncate());
+  }
+
+
+}
+
